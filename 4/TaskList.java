acee3b8cc418cb7b326d5c506c0402c3d4b115c9
natsgt <nrod@itu.dk>
2022-12-04 18:47:41 +0100

    change logic, commpare numbers

--- 4/TaskList.java
+++ 4/TaskList.java
@@ -3,43 +3,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TaskList {
-    private List<List<String>> tasks;
+    private List<List<String[]>> tasks;
 
     public TaskList() {
         tasks = new ArrayList<>();
     }
 
     public void addItem(String input) {
-        List<String> group = new ArrayList<>();
+        List<String[]> group = new ArrayList<>();
         var separatedString = input.split(",");
-        String firstString = getRange(separatedString[0]);
-        String secondString = getRange(separatedString[1]);
-        // System.out.println(firstString + " " + secondString);
-        group.add(firstString);
-        group.add(secondString);
+        String[] firstRange = getLimits(separatedString[0]);
+        String[] secondRange = getLimits(separatedString[1]);
+        group.add(firstRange);
+        group.add(secondRange);
         tasks.add(group);
     }
 
-    public String getRange(String rangeString) {
-        String num = "";
-        String[] twoNumber = rangeString.split("-");
-        for (int i = Integer.parseInt(twoNumber[0]); i <= Integer.parseInt(twoNumber[1]); i++) {
-            num += i;
-        }
+    public String[] getLimits(String rangeString) {
+        String[] num = new String[1];
+        num = rangeString.split("-");
         return num;
     }
 
     public int countRepeated() {
         int count = 0;
-        for (List<String> list : tasks) {
+        for (List<String[]> list : tasks) {
             var firstList = list.get(0);
             var secondList = list.get(1);
-            if (firstList.contains(secondList)) {
+            var firstLimit = Integer.parseInt(firstList[0]);
+            var firstEndLimit = Integer.parseInt(firstList[1]);
+            var secondLimit = Integer.parseInt(secondList[0]);
+            var secondEndLimit = Integer.parseInt(secondList[1]);
+            if (firstLimit >= secondLimit && firstEndLimit <= secondEndLimit) {
                 count++;
-                System.out.println(secondList + " count:" + count + " line " + tasks.indexOf(list));
-            } else if (secondList.contains(firstList)) {
+            } else if (secondLimit >= firstLimit && secondEndLimit <= firstEndLimit) {
                 count++;
-                System.out.println(firstList + " count:" + count + " line " + tasks.indexOf(list));
             }
         }
         return count;
